60b28cc41fc2a09010828a5d8e7a03f2d0332611
Nithin n sibi <113897873+nithin403@users.noreply.github.com>
2023-02-06 11:16:39 +0530

    Add files via upload

---
+++ lab2.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+public class lab2 {
+  static Scanner sc = new Scanner(System.in);
+  static String[] names = new String[1024];
+  static int count = 0;
+
+  public static void main(String[] args) {
+    int option;
+    do {
+      printMenuDriven();
+        option = sc.nextInt();
+      switch (option) {
+        case 1:
+          addName();
+          break;
+        case 2:
+          searchName();
+          break;
+        case 3:
+          removeName();
+          break;
+        case 4:
+          System.out.println("Quit programme");
+          break;
+        default:
+          System.out.println("Please enter a valid option");
+          break;
+      }
+    } while (option != 4);
+  }
+
+  static void printMenuDriven() {
+    System.out.println("Menu:");
+    System.out.println("1. Enter a name");
+    System.out.println("2. Search for a name");
+    System.out.println("3. Remove a name");
+    System.out.println("4. Exit");
+    System.out.print("Enter your option: ");
+  }
+
+  static void addName() {
+    System.out.print("Enter name: ");
+    String name = sc.next();
+    if (isNameExists(name)) {
+      System.out.println("Name can't be repeated");
+    } else {
+      names[count++] = name;
+      System.out.println("Name added successfully");
+    }
+  }
+
+  static void searchName() {
+    System.out.print("search any name: ");
+    String name = sc.next();
+    boolean isFound = false;
+    for (int i = 0; i < count; i++) {
+      if (names[i].equals(name)) {
+        System.out.println("Name found at index: " + (i + 1));
+        isFound = true;
+        break;
+      }
+    }
+    if (!isFound) {
+      System.out.println("Name not found.");
+    }
+  }
+
+  static void removeName() {
+    System.out.print("Enter name to be removed: ");
+    String name = sc.next();
+    boolean isFound = false;
+    for (int i = 0; i < count; i++) {
+      if (names[i].equals(name)) {
+        for (int j = i; j < count - 1; j++) {
+          names[j] = names[j + 1];
+        }
+        count--;
+        System.out.println("Name removed successfully");
+        isFound = true;
+        break;
+      }
+    }
+    if (!isFound) {
+      System.out.println("Name not found.");
+    }
+  }
+
+  static boolean isNameExists(String name) {
+    for (int i = 0; i < count; i++) {
+      if (names[i].equals(name)) {
+        return true;
+      }
+    }
+    return false;
+  }
+    
+}
